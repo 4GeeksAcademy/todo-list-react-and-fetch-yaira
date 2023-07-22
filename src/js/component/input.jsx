16ebc33e977828963d8a6ada38af526de4de7e78
Yvv1214 <39809411+Yvv1214@users.noreply.github.com>
2023-07-22 03:49:43 +0000

    90%

--- src/js/component/input.jsx
+++ src/js/component/input.jsx
@@ -1,27 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import BottomPart from "./bottomPart";
 import Api from "../todoAPI";
 
 
 const InputBar = () => {
     const [todos, setTodos] = useState([])
+    const [obj, setObj] = useState({})
     console.log(todos);
     //creates new todo 
     //todoinput is the name of the input, 
     //in the IF() todo is saying 'cleandishes' and 'CLEANDISHES' is the same so isNew is false because its not a new thing
     // 
-
-    const addTodos = async (e) => {
-
-        e.preventDefault()
-        let newTodo = e.target.todoInput.value;
-        let isNew = true;        
-        todos.forEach(todo => {
-            if(todo.toLowerCase() === newTodo.toLowerCase()){
-                isNew =false
-            }
-        })
-        const response = await fetch("https://fake-todo-list-52f9a4ed80ce.herokuapp.com/todos/user/sombra", {
+    useEffect(() => {
+        fetch("https://assets.breatheco.de/apis/fake/todos/user/yvv")
+             //turns it into json format 
+               .then((response) =>{ 
+               return response.json()})
+             //then setpost value in to data
+               .then((data) => {
+               setTodos(setPosts(data));
+                 console.log(data);
+               })
+               .catch((error) => console.log(error));
+              } ,[]);
+      
+    useEffect(() => {
+         const response = fetch("https://fake-todo-list-52f9a4ed80ce.herokuapp.com/todos/user/yvv", {
           method:'PUT',
           headers:{'Content-type': 'application/json'},
           body:JSON.stringify(todos)
@@ -33,8 +37,10 @@ const InputBar = () => {
         .catch((error) => {
             console.log(error)
         })
-    }
 
+    },[todos])
+    
+       
     const createTask = (e) => {
         e.preventDefault()
         let newTodo = e.target.todoInput.value;
@@ -52,25 +58,13 @@ const InputBar = () => {
     const removeTodo = async (e, index) => {
         e.preventDefault();
         let filtered = todos.filter((todo, i) => i !== index);
-        const response = await fetch("https://fake-todo-list-52f9a4ed80ce.herokuapp.com/todos/user/sombra", {
-            method:'PUT',
-            headers:{'Content-type': 'application/json'},
-            body:JSON.stringify(filtered)
-          })
-          .then((response)=> {
-              response.status == 200 ? setTodos(filtered) : ''
-  
-          })
-          .catch((error) => {
-            console.log(error)
-        })
-
+        setTodos(filtered);
     }
 
     // using the <form> eliminates the need to make an enter function
     return(
         <div>
-            <form onSubmit={addTodos}>
+            <form onSubmit={createTask}>
             <input 
             className="input"
             name="todoInput" 
